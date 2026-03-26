e68e799f6d2515710b4b2848d120126d1a3be23e
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-03-26 16:50:20 +0200

    Kata 'Reversed Strings' solved

---
+++ src/main/java/ReversedStrings.java
@@ -0,0 +1,9 @@
+public class ReversedStrings {
+
+    public static String solution(String str) {
+        return new StringBuilder(str)
+                .reverse()
+                .toString();
+    }
+
+}
---
+++ src/test/java/ReversedStringsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReversedStringsTest {
+    @Test
+    public void sampleTests() {
+        assertEquals("dlrow", ReversedStrings.solution("world"));
+    }
+}
