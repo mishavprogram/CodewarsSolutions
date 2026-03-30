2db698d09e15f2a2662dbe27c5e2401cfa88ebcb
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-03-30 22:55:13 +0300

    Kata 'Keep Hydrated' solved

---
+++ src/main/java/KeepHydrated.java
@@ -0,0 +1,7 @@
+public class KeepHydrated {
+
+    public static int liters(double time) {
+        return (int) Math.round(time * 0.5 - 0.5);
+    }
+
+}
---
+++ src/test/java/KeepHydratedTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KeepHydratedTest {
+
+    @Test
+    @DisplayName("liters(2)")
+    public void basicTest1() {
+        assertEquals(1, KeepHydrated.liters(2));
+    }
+
+    @Test @DisplayName("liters(0.82)")
+    public void basicTest2()  {
+        assertEquals(0, KeepHydrated.liters(0.82));
+    }
+
+    @Test @DisplayName("liters(12.83)")
+    public void basicTest3()  {
+        assertEquals(6, KeepHydrated.liters(12.83));
+    }
+
+    @Test  @DisplayName("liters(1802.44)")
+    public void basicTest4()  {
+        assertEquals(901, KeepHydrated.liters(1802.44));
+    }
+
+    @Test @DisplayName("liters(60)")
+    public void basicTest5()  {
+        assertEquals(30, KeepHydrated.liters(60));
+    }
+
+    @Test @DisplayName("liters(60)")
+    public void basicTest6()  {
+        assertEquals(117, KeepHydrated.liters(235));
+    }
+
+}
