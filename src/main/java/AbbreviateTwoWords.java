da0279871a35e4050c72bbef140601cb97aa7591
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-04-03 22:51:23 +0300

    Kata 'Abbreviate Two Words' solved

---
+++ src/main/java/AbbreviateTwoWords.java
@@ -0,0 +1,14 @@
+public class AbbreviateTwoWords {
+
+    public static String abbrevName(String name) {
+        String[] fullName = name.split(" ");
+
+        String firstName = fullName[0];
+        String lastName = fullName[1];
+
+        return String.valueOf(firstName.charAt(0)).toUpperCase() +
+                "." +
+                String.valueOf(lastName.charAt(0)).toUpperCase();
+    }
+
+}
