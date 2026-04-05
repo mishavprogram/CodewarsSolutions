ade29c4870836d5a8e4638752e80e82e51f33e02
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-04-05 16:11:30 +0300

    Kata 'PowersOf2' solved

---
+++ src/main/java/PowersOf2.java
@@ -0,0 +1,13 @@
+public class PowersOf2 {
+
+    public static long[] powersOfTwo(int n) {
+        long[] result = new long[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            result[i] = (long) Math.pow(2, i);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/test/java/PowersOf2Test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class PowersOf2Test {
+
+    @Test
+    public void testSomething() {
+        assertArrayEquals(new long[]{1}, PowersOf2.powersOfTwo(0));
+        assertArrayEquals(new long[]{1,2}, PowersOf2.powersOfTwo(1));
+        assertArrayEquals(new long[]{1,2,4,8,16}, PowersOf2.powersOfTwo(4));
+    }
+
+}
