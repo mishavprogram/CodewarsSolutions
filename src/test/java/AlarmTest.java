eaf55077b2340fe59d30d3f58792ac1e1331c4cb
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-04-12 17:39:08 +0300

    Kata 'Alarm' solved

---
+++ src/test/java/AlarmTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class AlarmTest {
+
+    @Test
+    public void setAlarmTest() {
+        assertTrue(Alarm.setAlarm(true, false));
+        assertFalse(Alarm.setAlarm(true, true));
+        assertFalse(Alarm.setAlarm(false, false));
+        assertFalse(Alarm.setAlarm(false, true));
+    }
+
+}
